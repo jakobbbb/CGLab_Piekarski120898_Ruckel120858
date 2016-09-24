10b31f43b9c3794b1b804bb28eb65fa9852bc39f
wobakj <jakob.wagner@uni-weimar.de>
2016-09-24 23:45:54 +0200

    new initial commit, reset history

---
+++ application/include/application_solar.hpp
@@ -0,0 +1,34 @@
+#ifndef APPLICATION_SOLAR_HPP
+#define APPLICATION_SOLAR_HPP
+
+#include "application.hpp"
+#include "model.hpp"
+#include "structs.hpp"
+
+// gpu representation of model
+class ApplicationSolar : public Application {
+ public:
+  // allocate and initialize objects
+  ApplicationSolar(std::string const& resource_path);
+  // free allocated objects
+  ~ApplicationSolar();
+
+  // update uniform locations and values
+  void uploadUniforms();
+  // update projection matrix
+  void updateProjection();
+  // react to key input
+  void keyCallback(int key, int scancode, int action, int mods);
+  // draw all objects
+  void render() const;
+
+ protected:
+  void initializeShaderPrograms();
+  void initializeGeometry();
+  void updateView();
+
+  // cpu representation of model
+  model_object planet_object;
+};
+
+#endif
