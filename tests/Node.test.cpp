481d8bdab22d7f4fcb80d3457f3e1b675c25a4b1
lisa <science-cat@gmx.net>
2021-10-28 19:37:53 +0200

    Add tests for removeChildren()

--- tests/Node.test.cpp
+++ tests/Node.test.cpp
@@ -14,4 +14,18 @@ TEST_CASE("node initialization") {
         REQUIRE(child->getDepth() == 1);
         REQUIRE(child->getParent() == root);
     }
+
+    SECTION("remove one child") {
+        auto child = std::make_shared<Node>(root, "child");
+        auto nochild = std::make_shared<Node>(nullptr, "nochild");
+        REQUIRE(root->getDepth() == 0);
+        root->addChildren(child);
+        REQUIRE(root->getDepth() == 1);
+        root->removeChildren(child->getName());
+        REQUIRE(root->getDepth() == 0);
+        root->removeChildren(nochild->getName());
+        REQUIRE(root->getDepth() == 0);
+        REQUIRE_FALSE(root->getChildren(nochild->getName()));
+        REQUIRE_THROWS(root->removeChildren(nochild->getName()));
+    }
 }
