e36df2cb91896bf20b59ff53bb3f4c650bf45dca
lisa <science-cat@gmx.net>
2021-11-02 13:51:10 +0100

    application_solar: handle key input

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -171,6 +171,8 @@ void ApplicationSolar::initializeGeometry() {
 
 ///////////////////////////// callback functions for window events ////////////
 // handle key input
+// - press W / S == zoom in / out
+// - press A / D == move left / right
 void ApplicationSolar::keyCallback(int key, int action, int mods) {
   if (key == GLFW_KEY_W  && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
     m_view_transform = glm::translate(m_view_transform, glm::fvec3{0.0f, 0.0f, -0.1f});
@@ -180,6 +182,14 @@ void ApplicationSolar::keyCallback(int key, int action, int mods) {
     m_view_transform = glm::translate(m_view_transform, glm::fvec3{0.0f, 0.0f, 0.1f});
     uploadView();
   }
+  else if (key == GLFW_KEY_A && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
+		m_view_transform = glm::translate(m_view_transform, glm::fvec3{0.1f, 0.0f, 0.0f});
+		uploadView();
+	}
+  else if (key == GLFW_KEY_D && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
+		m_view_transform = glm::translate(m_view_transform, glm::fvec3{-0.1f, 0.0f, 0.0f});
+		uploadView();
+	}
 }
 
 //handle delta mouse movement input
