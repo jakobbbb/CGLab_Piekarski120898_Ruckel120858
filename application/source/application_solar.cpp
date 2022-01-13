d49bf4ee3c31a3b674f62157c6b6df321f3fd454
Jakob Ruckel <jakob@ruckel.de>
2022-01-13 18:01:11 +0100

    On resize: Resize textures, but keep framebuffer
    No need to delete and re-create the framebuffer, actually.

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -578,18 +578,19 @@ void ApplicationSolar::initializeSkyboxGeometry() {
 
 void ApplicationSolar::initializeFramebuffer() {  
 
-    if (glIsFramebuffer(fbo)) {
-        glDeleteFramebuffers(1, &fbo);
-        glDeleteTextures(1, &texture);
-        glDeleteRenderbuffers(1, &rbo);
-    }
+    bool fb_initialized = (bool) glIsFramebuffer(fbo);
 
     // generate and bind framebuffer object
-    glGenFramebuffers(1, &fbo);
+    if (!fb_initialized) {
+        glGenFramebuffers(1, &fbo);
+    }
     glBindFramebuffer(GL_FRAMEBUFFER, fbo);
     // generate and bind texture
-    glGenTextures(1, &texture);
+    if (!fb_initialized) {
+        glGenTextures(1, &texture);
+    }
     glBindTexture(GL_TEXTURE_2D, texture);
+
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, 
                  GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -599,7 +600,9 @@ void ApplicationSolar::initializeFramebuffer() {
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
                            GL_TEXTURE_2D, texture, 0);
     // define renderbuffer object
-    glGenRenderbuffers(1, &rbo);
+    if (!fb_initialized) {
+        glGenRenderbuffers(1, &rbo);
+    }
     glBindRenderbuffer(GL_RENDERBUFFER, rbo);
     // define a depth and stencil renderbuffer object
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
