63589bc5308eb7e7820ad06b7ca2c64c588452b6
Jakob Ruckel <jakob@ruckel.de>
2022-01-19 16:55:07 +0100

    Fix black seams in planets

--- framework/source/utils.cpp
+++ framework/source/utils.cpp
@@ -29,8 +29,8 @@ texture_object create_texture_object(pixel_data const& texture) {
     glBindTexture(GL_TEXTURE_2D, texture_handle);
 
     // parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
